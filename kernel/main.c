7256e8961dbda08346b3a735afbb379d258455a7
snow <snow@snow.com>
2019-09-03 21:45:27 +0800

    linear address

--- kernel/main.c
+++ kernel/main.c
@@ -15,6 +15,7 @@
 
 void Start_Kernel(void)
 {
+	//addr确定帧缓存区被映射的线性地址
 	int *addr = (int *)0xffff800000a00000;
 	int i;
 
@@ -24,8 +25,8 @@ void Start_Kernel(void)
 		*((char *)addr+0)=(char)0x00;
 		*((char *)addr+1)=(char)0x00;
 		*((char *)addr+2)=(char)0xff;
-		*((char *)addr+3)=(char)0x00;	
-		addr +=1;	
+		*((char *)addr+3)=(char)0x00;
+		addr +=1;
 	}
 	for(i = 0 ;i<1440*20;i++)
 	{
